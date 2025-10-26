5f093f99d59b66cf9355d528ea454daf0ec67b23
Zahra Alvandi <zalvandi39@gmail.com>
2025-10-26 15:54:43 +0330

    finally react & tailwind is here

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ProfileCard from './ProfileCard/profileCard';
+
+function App() {
+  return (
+   <div>
+        <ProfileCard></ProfileCard>
+   </div>
+  );
+}
+
+export default App;
